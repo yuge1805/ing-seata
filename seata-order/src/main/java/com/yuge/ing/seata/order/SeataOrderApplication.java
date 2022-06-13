4d9c8421549cc5261c28cbcb1090aaa74086be1c
yuge <yuge1805@yeah.net>
2022-06-13 22:27:56 +0800

    feat: init

---
+++ seata-order/src/main/java/com/yuge/ing/seata/order/SeataOrderApplication.java
@@ -0,0 +1,13 @@
+package com.yuge.ing.seata.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataOrderApplication.class, args);
+    }
+
+}
---
+++ seata-order/src/test/java/com/yuge/ing/seata/order/SeataOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuge.ing.seata.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeataOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
